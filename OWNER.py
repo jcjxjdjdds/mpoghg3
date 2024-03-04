8c91531f5bd0fe329202023786c3cafbce1f9b91
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-03-04 12:46:56 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["ALH_KAR", "@M_O_lp"]
+OWNER_NAME = "👿☠️𝘼 𝙡 𝙈 𝙖 𝙧 𝙚 𝙗⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ⧚آلَمِےـــــرعَےـــــبّ"
+BOT_TOKEN = "6875562036:AAHA02aA0LszliIr7RbkeLXGJmOeAS9klo4"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/HLV_M"
+GROUP = "https://t.me/ALTUNZAT"
+VIDEO = "https://t.me/HLV_M/634"
+LOGS = "SAGAL_botk"
+
